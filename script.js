78a4c458d7ccb06f4951b93957b3a11bf2492459
felipesantos5 <felipesantosmarcelino2004@gmail.com>
2022-07-25 09:19:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var imputpage1 = document.querySelector("#imput-page1");
+var imputpage2 = document.querySelector("#imput-page2");
+var imputpage3 = document.querySelector("#imput-page3");
+
+function RedirectPage1() {
+  window.location ="index.html"
+
+}
+
+function RedirectPage2() {
+  window.location ="page2.html"
+
+}
+
+function RedirectPage3() {
+  window.location ="page3.html"
+}
