444deb9973956931e57eb3952d596e35a5764dbc
felipesantos5 <felipesantosmarcelino2004@gmail.com>
2022-07-25 11:16:20 -0300

    change functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
+// redirects index.html :
+
+function RedirectIndex1() {
+  window.location ="index.html"
+
+}
+function RedirectIndex2() {
+  window.location ="./pages/page2.html"
+
+}
+function RedirectIndex3() {
+  window.location ="./pages/page3.html"
+
+}
+
+// redirects other pages :
+
 function RedirectPage1() {
   window.location ="../index.html"
 
 }
 
 function RedirectPage2() {
-  window.location ="/pages/page2.html"
+  window.location ="./papage2.html"
 
 }
 
 function RedirectPage3() {
-  window.location ="/pages/page3.html"
+  window.location ="page3.html"
 }
