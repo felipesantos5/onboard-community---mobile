0aae778c4cced39d68da938ba7fe98d2ce165ea4
felipesantos5 <felipesantosmarcelino2004@gmail.com>
2022-07-25 10:25:09 -0300

    style changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var imputpage1 = document.querySelector("#imput-page1");
-var imputpage2 = document.querySelector("#imput-page2");
-var imputpage3 = document.querySelector("#imput-page3");
-
 function RedirectPage1() {
   window.location ="index.html"
 
